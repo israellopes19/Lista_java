2e6c05229a0f5b6c50bb17c2fd8fcff6053b585f
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-08-07 21:14:36 -0300

    Add files via upload

--- LISTA ISRAEL/src/Quest3.java
+++ LISTA ISRAEL/src/Quest3.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Quest3 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.println("Digite seu saldo:");
+        double saldo = s.nextDouble();
 
+        double reajuste = 0.01 ;
+
+        double reajustar = (reajuste * saldo) + saldo;
+
+        System.out.printf(" O seu saldo e R$ %.2f" ,reajustar);
     }
 }
