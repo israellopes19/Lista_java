110d57f39de66d6d4f43c46d80e28488bdca9ac8
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-08-07 21:18:23 -0300

    Add files via upload

--- LISTA ISRAEL/src/Quest2.java
+++ LISTA ISRAEL/src/Quest2.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
+
 public class Quest2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        int n1 = 7;
+        int n2 = 8;
+        int n3 = 9;
 
+        int media = (n1+n2+n3) / 3;
 
+        System.out.print("Media é " +media);
 
     }
 }
