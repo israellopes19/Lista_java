500bb646cefa3088a185dd41912422e129e38a0a
Israellopes <143666711+israellopes19@users.noreply.github.com>
2024-08-09 19:40:20 -0300

    Add files via upload

--- LISTA ISRAEL/src/Quest2.java
+++ LISTA ISRAEL/src/Quest2.java
@@ -11,7 +11,19 @@ public class Quest2 {
 
         int media = (n1+n2+n3) / 3;
 
-        System.out.print("Media é " +media);
+        System.out.println("Media é " +media);
 
+        int n4 = 4;
+        int n5 = 5;
+        int n6 = 6;
+
+        int media2 = (n4+n5+n6) / 3;
+
+        System.out.println("Media é " +media2);
+
+
+        double soma =(double) (media + media2) / 2;
+
+        System.out.println("Media das medias " +soma);
     }
 }
